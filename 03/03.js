4b73201c57f4dc9f4986f4220675b1d5e0308991
his0822 <pnu40223@gmail.com>
2024-02-22 12:59:40 +0900

    vq

--- 03/03.js
+++ 03/03.js
@@ -1,3 +1,5 @@
+/* 함수작성 1*/
+/*
 function handleClick(n) {
     // 메세지 영역 가져오기1
     // document.getElementById("msgAria").innerHTML = "안녕하세요";
@@ -6,6 +8,9 @@ function handleClick(n) {
     // document.querySelector("#msgAria").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다.</h2>";
 
     document.querySelector("#msgAria").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다.</h2>`;
-    
-    
+}
+*/
+/*함수작성 2*/
+const handleClick = (n) => {
+    document.querySelector("#msgAria").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다.</h2>`;
 }
